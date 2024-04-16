cc95f6ac087f18de151363a58501440faab2cd82
Nesta-gitU <99024501+Nesta-gitU@users.noreply.github.com>
2024-04-16 18:00:01 +0200

    start with rotMnist loader

---
+++ src/data/rotated_mnist_datamodule.py
@@ -0,0 +1,239 @@
+from typing import Any, Dict, Optional, Tuple
+
+import torch
+from lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
+from torchvision.datasets import MNIST
+from torchvision.transforms import transforms
+
+##imports for second class
+from torchvision.transforms import RandomRotation
+from torchvision.transforms import Pad
+from torchvision.transforms import Resize
+from torchvision.transforms import ToTensor
+from torchvision.transforms import Compose
+from torchvision.transforms import InterpolationMode
+
+import numpy as np
+
+from PIL import Image
+
+import requests
+import zipfile
+import os
+
+
+class RotatedMNISTDataModule(LightningDataModule):
+    """`LightningDataModule` for the MNIST dataset.
+    """
+
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+        transforms = None
+    ) -> None:
+        """Initialize a `MNISTDataModule`.
+
+        :param data_dir: The data directory. Defaults to `"data/"`.
+        :param train_val_test_split: The train, validation and test split. Defaults to `(55_000, 5_000, 10_000)`.
+        :param batch_size: The batch size. Defaults to `64`.
+        :param num_workers: The number of workers. Defaults to `0`.
+        :param pin_memory: Whether to pin memory. Defaults to `False`.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+        
+        self.transforms = transforms
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+        self.batch_size_per_device = batch_size
+
+    @property
+    def num_classes(self) -> int:
+        """Get the number of classes.
+
+        :return: The number of RotMNIST classes (10).
+        """
+        return 10
+
+    def prepare_data(self) -> None:
+        """Download data if needed. Lightning ensures that `self.prepare_data()` is called only
+        within a single process on CPU, so you can safely add your downloading logic within. In
+        case of multi-node training, the execution of this hook depends upon
+        `self.prepare_data_per_node()`.
+
+        Do not use it to assign state (self.x = y).
+        """
+        MnistRotDataset(self.hparams.data_dir, train=True, download=True)
+        MnistRotDataset(self.hparams.data_dir, train=False, download=True)
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by Lightning before `trainer.fit()`, `trainer.validate()`, `trainer.test()`, and
+        `trainer.predict()`, so be careful not to execute things like random split twice! Also, it is called after
+        `self.prepare_data()` and there is a barrier in between which ensures that all the processes proceed to
+        `self.setup()` once the data is prepared and available for use.
+
+        :param stage: The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`. Defaults to ``None``.
+        """
+        # Divide batch size by the number of devices.
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            trainset = MnistRotDataset(self.hparams.data_dir, train=True, transform=self.transforms)
+            testset = MnistRotDataset(self.hparams.data_dir, train=False, transform=self.transforms)
+            dataset = ConcatDataset(datasets=[trainset, testset])
+
+            #I changed this because I want to keep the same test set as them I think instead of random? maybe not TODO
+            self.data_train, self.data_val, self.data_test = random_split(
+                dataset=dataset,
+                lengths=self.hparams.train_val_test_split,
+                generator=torch.Generator().manual_seed(42),
+            )
+
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader.
+
+        :return: The train dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader.
+
+        :return: The validation dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader.
+
+        :return: The test dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def teardown(self, stage: Optional[str] = None) -> None:
+        """Lightning hook for cleaning up after `trainer.fit()`, `trainer.validate()`,
+        `trainer.test()`, and `trainer.predict()`.
+
+        :param stage: The stage being torn down. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
+            Defaults to ``None``.
+        """
+        pass
+
+    def state_dict(self) -> Dict[Any, Any]:
+        """Called when saving a checkpoint. Implement to generate and save the datamodule state.
+
+        :return: A dictionary containing the datamodule state that you want to save.
+        """
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        """Called when loading a checkpoint. Implement to reload datamodule state given datamodule
+        `state_dict()`.
+
+        :param state_dict: The datamodule state returned by `self.state_dict()`.
+        """
+        pass
+
+
+class MnistRotDataset(Dataset):
+    """
+    Implements rotMnist like MNIST in the other file
+    -> I implented in a way where you dont have to give a datadir 
+    """
+    
+    def __init__(self, root, train = True, transform = None, download = False):
+        if download == True:
+            self.download(root)
+        
+        if train:
+            file = "mnist_rotation_new/mnist_all_rotation_normalized_float_train_valid.amat"
+        else:
+            file = "mnist_rotation_new/mnist_all_rotation_normalized_float_test.amat"
+        
+        self.transform = transform
+
+        file = os.path.join(root, file)
+
+        data = np.loadtxt(file, delimiter=' ')
+            
+        self.images = data[:, :-1].reshape(-1, 28, 28).astype(np.float32)
+        self.labels = data[:, -1].astype(np.int64)
+        self.num_samples = len(self.labels)
+    
+    def __getitem__(self, index):
+        image, label = self.images[index], self.labels[index]
+        image = Image.fromarray(image, mode='F')
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
+    
+    def __len__(self):
+        len(self.labels)
+    
+    def download(self, root):
+        # URL of the dataset
+        url = "http://www.iro.umontreal.ca/~lisa/icml2007data/mnist_rotation_new.zip"
+        # Destination directory
+        destination_dir = os.path.join(root, "mnist_rotation_new")
+
+        # Check if the final unzipped folder exists
+        if not os.path.exists(destination_dir):
+
+            # Check if the directory already exists, if not, create it
+            os.makedirs(destination_dir)
+
+            # Download the dataset
+            filename = os.path.join(destination_dir, "mnist_rotation_new.zip")
+            if not os.path.exists(filename):
+                print("Downloading dataset...")
+                response = requests.get(url)
+                with open(filename, "wb") as f:
+                    f.write(response.content)
+                print("Download completed.")
+
+            # Unzip the dataset
+            print("Unzipping dataset...")
+            with zipfile.ZipFile(filename, "r") as zip_ref:
+                zip_ref.extractall(destination_dir)
+            print("Unzip completed.")
+
+
+if __name__ == "__main__":
+    _ = RotatedMNISTDataModule()
--- src/train.py
+++ src/train.py
@@ -84,7 +84,7 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
 
     if cfg.get("train"):
         log.info("Starting training!")
-        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path")) ##TODO adjust this to checkpoint more often...
 
     train_metrics = trainer.callback_metrics
 
