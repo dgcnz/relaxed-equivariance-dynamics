154764c6aa3e70b477b4614ce881658f6e5e02b0
Diego Canez <canezdiego@gmail.com>
2024-05-13 15:37:51 +0200

    fix: reduce cpu on test echo slurm script

--- scripts/slurm/echo.sh
+++ scripts/slurm/echo.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus=1
 #SBATCH --job-name=DL2
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=1
 #SBATCH --time=00:00:05
 #SBATCH --output=scripts/slurm_logs/slurm_output_%A.out
 
