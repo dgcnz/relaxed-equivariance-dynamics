c3e6d732534ce7801542cdb49c1ed399bafb542c
Diego Canez <canezdiego@gmail.com>
2024-05-13 15:53:53 +0200

    fix: script executable test_train

--- scripts/slurm/train_test.sh
+++ scripts/slurm/train_test.sh
@@ -5,11 +5,11 @@
 #SBATCH --job-name=DL2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=00:00:10
+#SBATCH --time=00:05:00
 #SBATCH --output=scripts/slurm_logs/slurm_output_%A.out
 
 cd $HOME/development/dl2
 source .venv/bin/activate
 # run script from above
-srun python -m src.train --experiment=configs/experiment/wang2022/equivariance_test/convnet trainer.max_epochs=1
+srun python -m src.train experiment=wang2022/equivariance_test/convnet trainer.max_epochs=1
 deactivate
