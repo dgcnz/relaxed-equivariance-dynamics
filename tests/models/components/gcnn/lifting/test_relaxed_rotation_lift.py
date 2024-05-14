b34d63387619b5613cb65826afab6153e902d225
Diego Canez <canezdiego@gmail.com>
2024-05-14 15:55:27 +0200

    feat: test equivalence of Wang's relaxed lifting implementation and gconv's

--- tests/models/components/gcnn/lifting/test_relaxed_rotation_lift.py
+++ tests/models/components/gcnn/lifting/test_relaxed_rotation_lift.py
@@ -2,6 +2,9 @@ import torch
 import pytest
 from src.models.components.gcnn.lifting.relaxed_rotation import RLiftingConvCn
 from src.models.components.gcnn.lifting.relaxed_rotation_rui import RuiRLiftingConvCn
+import math
+from gconv import gnn
+
 
 @pytest.mark.skip
 @pytest.mark.parametrize("group_order", [2, 4])
@@ -23,7 +26,6 @@ def test_relaxed_lifting_forward_even(group_order: int):
 
     assert y1.shape == y2.shape
     assert torch.allclose(y1, y2)
-
 
 
 @pytest.mark.skip
@@ -66,6 +68,7 @@ def test_einsum_equivalence_lifting():
     # Check that the two results are close
     assert torch.allclose(result_einsum, result_fast, atol=1e-6)
 
+
 @pytest.mark.skip
 @pytest.mark.parametrize("device", ["cpu", "mps"])
 @pytest.mark.parametrize("group_order", [16])
@@ -99,6 +102,7 @@ def test_benchmark_einsum(
         return torch.einsum("na, noa... -> oa...", relaxed_weights, filter_bank)
 
     benchmark(einsum)
+
 
 @pytest.mark.skip
 @pytest.mark.parametrize("device", ["cpu", "mps"])
@@ -146,3 +150,53 @@ def test_benchmark_fast(
     # Run the einsum operation
     # result_einsum: [out_channels, group_order, in_channels, kernel_size, kernel_size]
     benchmark(fast)
+
+
+@pytest.mark.parametrize("in_channels", [1, 3])
+@pytest.mark.parametrize("out_channels", [1, 3])
+@pytest.mark.parametrize("group_order", [2, 4])
+@pytest.mark.parametrize("kernel_size", [3, 5])
+@pytest.mark.parametrize("num_filter_banks", [1, 2])
+def test_gconv_lifting_forward_even(
+    kernel_size: int,
+    group_order: int,
+    out_channels: int,
+    in_channels: int,
+    num_filter_banks: int,
+):
+    x = torch.randn(1, in_channels, 28, 28)
+    torch.manual_seed(0)
+    # For some reason the kernel is rotated in the opposite direction
+    rui_grid_H = torch.tensor(
+        [
+            math.pi * 2 / group_order * ((group_order - i) % group_order)
+            for i in range(group_order)
+        ]
+    ).unsqueeze(-1)
+    num_filter_banks = 2
+    lift1 = RuiRLiftingConvCn(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        kernel_size=kernel_size,
+        group_order=group_order,
+        num_filter_banks=num_filter_banks,
+        activation=False,
+    )
+    lift2 = gnn.RGLiftingConvSE2(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        num_filter_banks=num_filter_banks,
+        kernel_size=kernel_size,
+        group_kernel_size=group_order,
+        padding=kernel_size // 2,
+        sampling_mode="nearest",
+        permute_output_grid=False,
+        mask=False,
+        grid_H=rui_grid_H,
+    )
+    # For some reason gnn transposes the kernel, not really important
+    lift1.kernel = torch.nn.Parameter(lift2.kernel.weight.transpose(-1, -2))
+
+    y1 = lift1(x)
+    y2, _ = lift2(x)
+    assert torch.allclose(y1, y2)
