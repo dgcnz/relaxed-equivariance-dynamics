4b96d3d359a4e5bb455b6963cb7974decb06fcda
Diego Canez <canezdiego@gmail.com>
2024-05-16 18:25:25 +0200

    feat: add wandb.get_model_and_data_modules_from_config

--- src/utils/wandb.py
+++ src/utils/wandb.py
@@ -1,6 +1,9 @@
 import wandb
 from wandb.apis.public import Run
 from omegaconf import OmegaConf, DictConfig
+import hydra
+from lightning import LightningModule, LightningDataModule
+
 
 def get_all_checkpoints(run_id: str, project: str, entity: str) -> list[str]:
     """Get all checkpoints from a run.
@@ -16,21 +19,22 @@ def get_all_checkpoints(run_id: str, project: str, entity: str) -> list[str]:
 
 
 def download_artifact(run: Run, artifact_name: str, project: str, entity: str) -> str:
-    """ Download artifact from wandb, link it to current run and return the path. 
+    """Download artifact from wandb, link it to current run and return the path.
     :param run: The current run. Wandb will automatically link the artifact to this run.
     :param artifact_name: The artifact name.
     :param project: The project name.
     """
-    artifact = run.use_artifact(f"{entity}/{project}/{artifact_name}", type='model')
+    artifact = run.use_artifact(f"{entity}/{project}/{artifact_name}", type="model")
     return artifact.download()
 
+
 def download_config_file(entity: str, project: str, run_id: str) -> DictConfig:
-    """ Download a config file from wandb and return the path. 
+    """Download a config file from wandb and return the path.
     :param entity: The entity name.
     :param project: The project name.
     :param run_id: The run id.
 
-    :return: The config file as a DictConfig. Keys: ["model", "data", "trainer", "callbacks", etc.]
+    :return: The config file as a DictConfig. Keys: ["model", "data", "trainer", "callbacks", "tags", etc.]
     """
     api = wandb.Api()
     run = api.run(f"{entity}/{project}/{run_id}")
@@ -38,3 +42,15 @@ def download_config_file(entity: str, project: str, run_id: str) -> DictConfig:
     fp = file.download(replace=True)
     conf = OmegaConf.load(fp)
     return conf
+
+
+def get_model_and_data_modules_from_config(
+    wandb_config: DictConfig,
+) -> tuple[LightningModule, LightningDataModule]:
+    """
+    Instantiates the model and datamodule from a config file downloaded from wandb.
+    :param wandb_config: The config file downloaded by `download_config_file`.
+    """
+    model = hydra.utils.instantiate(wandb_config.model.value)
+    datamodule = hydra.utils.instantiate(wandb_config.data.value)
+    return model, datamodule
