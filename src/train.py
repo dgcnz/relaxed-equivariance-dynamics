e5fc85cebf4999e8d219f8e60536cfe9c0f223f1
Nesta-gitU <99024501+Nesta-gitU@users.noreply.github.com>
2024-05-17 19:13:37 +0200

    not pl but L

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ import torch
 from lightning import Callback, LightningDataModule, LightningModule, Trainer
 from lightning.pytorch.loggers import Logger
 from omegaconf import DictConfig
-from pytorch_lightning.callbacks import ModelCheckpoint
+from lightning.callbacks import ModelCheckpoint
 from omegaconf import OmegaConf, ListConfig
 
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
