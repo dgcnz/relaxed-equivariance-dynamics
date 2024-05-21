63bff3416968eb450e21f328a89e8eac4dae3956
Diego Canez <canezdiego@gmail.com>
2024-05-21 13:19:24 +0200

    feat: add oh_t3 models

--- tests/models/components/wang2024/test_oh_t3.py
+++ tests/models/components/wang2024/test_oh_t3.py
@@ -1,67 +1,9 @@
 from gconv.gnn import GSeparableConvSE3, GLiftingConvSE3, GConvSE3, RGLiftingConvSE3
 from gconv.geometry.groups import so3
-from src.models.components.wang2024.oh_t3 import GCNNOhT3
+from src.models.components.wang2024.oh_t3 import GCNNOhT3, RGCNNOhT3
 import torch
 import pytest
 
-
-def test_oh_separable_conv():
-    H0 = so3.quat_to_matrix(so3.octahedral())
-    in_channels, hidden_dim, kernel_size = 3, 3, 3
-
-    lift = GLiftingConvSE3(
-        in_channels=in_channels,
-        out_channels=hidden_dim,
-        kernel_size=kernel_size,
-        groups=1,
-        padding=kernel_size // 2,
-        grid_H=H0,
-        permute_output_grid=False,
-        mask=False,
-    )
-    gsconv = GSeparableConvSE3(
-        in_channels=hidden_dim,
-        out_channels=hidden_dim,
-        kernel_size=kernel_size,
-        groups=1,
-        padding=kernel_size // 2,
-        permute_output_grid=False,
-        grid_H=H0,
-        mask=False,
-    )
-    gconv = GConvSE3(
-        in_channels=hidden_dim,
-        out_channels=hidden_dim,
-        kernel_size=kernel_size,
-        groups=1,
-        padding=kernel_size // 2,
-        permute_output_grid=False,
-        grid_H=H0,
-        mask=False,
-    )
-    x = torch.randn(1, in_channels, 48, 48, 48)
-    z, H1 = lift(x)
-    ws, H2s = gsconv(z, H1) 
-    w, H2 = gconv(z, H1) 
-    print(ws.shape, w.shape)
-
-def test_oh_lifting_rconv():
-    H0 = so3.quat_to_matrix(so3.octahedral())
-    in_channels, hidden_dim, kernel_size, num_filter_banks = 3, 3, 3, 6
-    lift = RGLiftingConvSE3(
-        in_channels=in_channels,
-        out_channels=hidden_dim,
-        num_filter_banks=num_filter_banks,
-        kernel_size=kernel_size,
-        groups=1,
-        padding=kernel_size // 2,
-        grid_H=H0,
-        permute_output_grid=False,
-        mask=False,
-    )
-
-    x = torch.randn(1, in_channels, 48, 48, 48)
-    z, H1 = lift(x)
 
 
 @pytest.mark.parametrize("classifier", [False, True])
@@ -70,6 +12,28 @@ def test_ohgcnn(classifier: bool):
     model = GCNNOhT3(
         in_channels=in_channels,
         out_channels=out_channels,
+        kernel_size=kernel_size,
+        hidden_dim=hidden_dim,
+        num_gconvs=num_gconvs,
+        classifier=classifier,
+        sigmoid=True,
+    )
+    x = torch.randn(1, in_channels, 64, 64, 64)
+    y = model(x)
+    if classifier:
+        assert y.shape == (1, out_channels)
+    else:
+        assert y.shape == (1, out_channels, 24, 64, 64, 64)
+
+
+@pytest.mark.parametrize("classifier", [False, True])
+def test_ohrgcnn(classifier: bool):
+    in_channels, out_channels, kernel_size, hidden_dim, num_gconvs = 3, 3, 3, 3, 3
+    num_filter_banks = 2
+    model = RGCNNOhT3(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        num_filter_banks=num_filter_banks,
         kernel_size=kernel_size,
         hidden_dim=hidden_dim,
         num_gconvs=num_gconvs,
