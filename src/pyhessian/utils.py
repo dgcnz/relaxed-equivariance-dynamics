"fix" hessian

src/pyhessian/utils.py
@@ -67,8 +67,10 @@ def get_params_grad(model):
     for param in model.net.parameters():
         if not param.requires_grad:
             continue
-        params.append(param)
-        grads.append(torch.tensor(0.) if param.grad is None else param.grad + 0.)
+        if param.grad is not None:
+            #print(param)
+            params.append(param)
+            grads.append(torch.tensor(0.) if param.grad is None else param.grad + 0.)
     return params, grads
 
 
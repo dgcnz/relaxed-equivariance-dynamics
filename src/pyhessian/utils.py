452a34090351db64d21db7a353f63cc1aa369895
Nesta-gitU <99024501+Nesta-gitU@users.noreply.github.com>
2024-05-21 19:45:16 +0200

    "fix" hessian

--- src/pyhessian/utils.py
+++ src/pyhessian/utils.py
@@ -67,8 +67,10 @@ def get_params_grad(model):
     for param in model.net.parameters():
         if not param.requires_grad:
             continue
-        params.append(param)
-        grads.append(torch.tensor(0.) if param.grad is None else param.grad + 0.)
+        if param.grad is not None:
+            #print(param)
+            params.append(param)
+            grads.append(torch.tensor(0.) if param.grad is None else param.grad + 0.)
     return params, grads
 
 
