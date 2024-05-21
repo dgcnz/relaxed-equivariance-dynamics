potentially fix hessian specturm

src/metrics/hessian_spectrum.py
@@ -9,9 +9,11 @@ def get_spectrum(cfg: DictConfig, config, datamodule, model) -> List:
     dataset = datamodule.data_train
 
     # get criterion (might have to make this selectable in the future)
-    loss_fn = torch.nn.CrossEntropyLoss()
-
-    #weight_decay = 1e-5
+    loss_fn = torch.nn.MSELoss()
+    if cfg.get("wang2022_loss"):
+        loss_fn = summed_mse_loss(cfg, model)
+    else:
+        ValueError("No loss function specified")
 
     spectrum = get_hessian_max_spectrum(
         model=model,
@@ -28,3 +30,30 @@ def get_spectrum(cfg: DictConfig, config, datamodule, model) -> List:
     )
 
     return spectrum
+
+def summed_mse_loss(cfg, model):
+        
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    with_weight_constraint = hasattr(model.net, "get_weight_constraint")
+    criterion = torch.nn.MSELoss()
+    
+    def loss_fn(xx, yy): #input, target
+        mse = torch.tensor(0.0, device=device)
+        
+        for y in yy.transpose(0, 1):
+            im = model.forward(xx)
+            xx = torch.cat([xx[:, im.shape[1] :], im], 1)
+            mse += criterion(im, y)
+
+        if with_weight_constraint:
+            weight_constraint = criterion(model.net.get_weight_constraint(), torch.tensor(0).float().cuda())
+            
+            loss = mse + weight_constraint
+        else:
+            weight_constraint = None
+            loss = mse
+
+        return loss
+    
+    return loss_fn