a5e78d9e120d7ef96f1fd7e686c18f90b5d60a47
MeneerTS <stessenthijs@gmail.com>
2024-05-22 13:43:29 +0200

    Made GCNNOhT3 like Wang 2024

--- src/models/components/wang2024/oh_t3.py
+++ src/models/components/wang2024/oh_t3.py
@@ -16,17 +16,20 @@ class GCNNOhT3(nn.Module):
         out_channels: int,
         kernel_size: int,
         hidden_dim: int,
-        num_gconvs: int,
+        num_blocks: int,
         classifier: bool = False,
         sigmoid: bool = False,
+        
     ):
-        assert num_gconvs >= 2
+        
         super().__init__()
 
         self.classifier = classifier
         self.sigmoid = sigmoid
         self.grid_Oh = so3.quat_to_matrix(so3.octahedral())
-        self.dims = [in_channels] + [hidden_dim] * (num_gconvs - 2) + [out_channels]
+        #self.dims = [in_channels] + [hidden_dim] * (num_gconvs - 2) + [out_channels]
+        self.upsampler = nn.Upsample(scale_factor=2,mode='trilinear')
+
         self.lift = GLiftingConvSE3(
             in_channels=in_channels,
             out_channels=hidden_dim,
@@ -36,34 +39,143 @@ class GCNNOhT3(nn.Module):
             permute_output_grid=False,
             mask=False,
         )
-        self.gconvs = nn.ModuleList(
-            [
-                GSeparableConvSE3(
+        
+        self.upconv_block1 = nn.ModuleList([
+            GSeparableConvSE3(
                     in_channels=hidden_dim,
-                    out_channels=self.dims[i],
+                    out_channels=hidden_dim,
                     kernel_size=kernel_size,
                     padding=kernel_size // 2,
                     permute_output_grid=False,
                     grid_H=self.grid_Oh,
                     mask=False,
-                )
-                for i in range(1, num_gconvs)
-            ]
-        )
+                    conv_mode='3d_transpose',
+                    output_padding=1,
+                    stride=2,
+                ),
+            GSeparableConvSE3(
+                    in_channels=hidden_dim,
+                    out_channels=hidden_dim,
+                    kernel_size=kernel_size,
+                    padding=kernel_size // 2,
+                    permute_output_grid=False,
+                    grid_H=self.grid_Oh,
+                    mask=False,                    
+                ),
+
+        ])
+
+        self.upconv_block2 = nn.ModuleList([
+            GSeparableConvSE3(
+                    in_channels=hidden_dim,
+                    out_channels=hidden_dim,
+                    kernel_size=kernel_size,
+                    padding=kernel_size // 2,
+                    permute_output_grid=False,
+                    grid_H=self.grid_Oh,
+                    mask=False,
+                    conv_mode='3d_transpose',
+                    output_padding=1,
+                    stride=2,
+                ),
+            GSeparableConvSE3(
+                    in_channels=hidden_dim,
+                    out_channels=hidden_dim,
+                    kernel_size=kernel_size,
+                    padding=kernel_size // 2,
+                    permute_output_grid=False,
+                    grid_H=self.grid_Oh,
+                    mask=False,                    
+                ),
+
+        ])
+
+        self.conv_block1 = nn.ModuleList([
+            GSeparableConvSE3(
+                    in_channels=hidden_dim,
+                    out_channels=hidden_dim,
+                    kernel_size=kernel_size,
+                    padding=kernel_size // 2,
+                    permute_output_grid=False,
+                    grid_H=self.grid_Oh,
+                    mask=False,
+                ),
+            GSeparableConvSE3(
+                in_channels=hidden_dim,
+                out_channels=hidden_dim,
+                kernel_size=kernel_size,
+                padding=kernel_size // 2,
+                permute_output_grid=False,
+                grid_H=self.grid_Oh,
+                mask=False,
+            )
+
+        ])
+        self.conv_block2 = nn.ModuleList([
+            GSeparableConvSE3(
+                    in_channels=hidden_dim,
+                    out_channels=hidden_dim,
+                    kernel_size=kernel_size,
+                    padding=kernel_size // 2,
+                    permute_output_grid=False,
+                    grid_H=self.grid_Oh,
+                    mask=False,
+                ),
+            GSeparableConvSE3(
+                in_channels=hidden_dim,
+                out_channels=hidden_dim,
+                kernel_size=kernel_size,
+                padding=kernel_size // 2,
+                permute_output_grid=False,
+                grid_H=self.grid_Oh,
+                mask=False,
+            )
+
+        ])
+
+        self.final_conv = GSeparableConvSE3(
+                in_channels=hidden_dim,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                padding=kernel_size // 2,
+                permute_output_grid=False,
+                grid_H=self.grid_Oh,
+                mask=False,
+            )
+        
+        
 
     def forward(self, x: Tensor) -> Tensor:
         """
         Forward pass of the group convolution layer
         :param x: input tensor of shape [B, #in, H, W]
         """
-        z, H = self.lift(x, self.grid_Oh)
-        for gconv in self.gconvs:
-            z, H = gconv(z, H)
+        z1, H1 = self.lift(x, self.grid_Oh)        
+
+        #The H's never scale so we don't need to upsample them
+        #Do we need to add them to the skip connection though?
+        z2,H2 = self.upconv_block1(z1,H1)
+        z2 += self.upsampler(z1)
+
+        z3,H3 = self.conv_block1(z2,H2)        
+        z3 += z2
+
+        z4, H4 = self.upconv_block2(z3,H3)
+        z4 += self.upsampler(z3)
+
+        z5, H5 = self.conv_block2(z4,H4)
+        z5 += z4
+
+        z6, H6 = self.final_conv(z5,H5)
+        
+
+        #These parts might need some pooling or whatever
+        #Like in the gconv tutorial
         if self.classifier:
-            z = z.mean((2, 3, 4, 5))
+            z6 = z6.mean((2, 3, 4, 5))
         if self.sigmoid:
-            z = z.sigmoid()
-        return z
+            z6 = z6.sigmoid()
+        return z6
 
 
 class RGCNNOhT3(nn.Module):
