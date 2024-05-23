fe84d64c9e73d1cc2e5574cf97025c78e5030caf
JiapengFan <64084916+JiapengFan@users.noreply.github.com>
2024-05-23 12:23:41 +0200

    put rotated model def outside loop

--- src/metrics/lie_derivative.py
+++ src/metrics/lie_derivative.py
@@ -5,14 +5,14 @@ def get_lie_equiv_err(model, datamodule, type):
     equiv_err = 0
 
     if type == 'rotation':
-        for (inp_imgs, _) in datamodule.get_testloader():
-            def rotated_model(t):
-                rotated_img = rotate(inp_imgs, t)
-                z = model(rotated_img)
-                if img_like(z.shape):
-                    z = rotate(z, -t)
-                return z
+        def rotated_model(t):
+            rotated_img = rotate(inp_imgs, t)
+            z = model(rotated_img)
+            if img_like(z.shape):
+                z = rotate(z, -t)
+            return z
 
+        for (inp_imgs, _) in datamodule.get_testloader():
             t = torch.zeros(1, requires_grad=True, device=inp_imgs.device)
             lie_deriv = jvp(rotated_model, t, torch.ones_like(t))
             equiv_err += torch.norm(lie_deriv.view(lie_deriv.size(0), -1), dim=(1)).mean()/(len(lie_deriv.size())-1)
