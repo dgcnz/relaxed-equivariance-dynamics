8560703478443da92b31e686b71287408e0f1daa
Nesta-gitU <99024501+Nesta-gitU@users.noreply.github.com>
2024-05-24 15:49:24 +0200

    fix sharpness

--- src/metrics/sharpness.py
+++ src/metrics/sharpness.py
@@ -1,57 +1,76 @@
 import torch
 import numpy as np
+import copy
 
-def get_sharpness(nn_model, datamodule, device):
+def get_sharpness(model, datamodule, device):
     '''
     Calculates the sharpness of the loss landscape.
     
     Returns:
     float: The sharpness of the loss landscape as defined in the blog post.
     '''
+    
 
     # List of magnitudes 
     T = torch.tensor(np.arange(0.1, 1.01, 0.3)).to(device)
 
-    nn_model.eval()
-    # Save the original state of the model
-    original_state_dict = nn_model.state_dict()
+    
+    # Save the original state of the mode
     
     sharpness = 0
     num_samples = 0
 
-    for (x, y) in datamodule.train_dataloader():
+    for (x ,y) in datamodule.train_dataloader():
+        nn_model = copy.deepcopy(model)
+        nn_model.eval()
+
         sharpness_batch = 0
         num_samples += x.shape[0]
 
-        x, y = x.to(device), y.to(device)
+        batch = (x.to(device), y.to(device))
+
+        ### CODE IS WRONG BECAUSE WE SHOULD FIRST DO A FULL MODEL PERTUBATION, AND ONLY THEN DO THE BATCH SHARPENESS
 
         # Calculate loss at w
         with torch.no_grad():
-            predictions = nn_model(x)
-            loss_w = torch.sqrt(torch.mean((predictions - y) ** 2))
+            _, _, loss_w, _ = model.model_step(batch) #always the og model
+            
+            for t in T:
+                for i in range(5):  #5 is the amount of directions we perturb in 
+                    nn_model = perturb_model(model, t)
+            
+            _,_,loss_perturbed,_ = nn_model.model_step(batch)
+            
+            print('w', loss_w)
+            print('perturbed', loss_perturbed)
 
-            for param in nn_model.parameters():
-                if param.requires_grad:
-                    # Generate random unit vectors of the same shape as param
-                    D = torch.randn(5, *param.shape).to(param.device)
-                    # Unsqueeze norm multiple times so that division is done per entry for the first dim of D.
-                    D = D / torch.norm(D.view(D.size(0), -1), dim=(1))[(..., ) + (None, ) * (len(D.size())-1)]
-                    
-                    for t in T:
-                        for d in range(D.shape[0]):
-                            # Apply the perturbation
-                            param.add_(t*D[d])
-
-                            predictions_perturbed = nn_model(x)
-                            loss_perturbed = torch.sqrt(torch.mean((predictions_perturbed - y) ** 2))
-                            sharpness_batch += abs(loss_perturbed - loss_w)
-                            
-                            nn_model.load_state_dict(original_state_dict)
-                    
-                    sharpness_batch /= D.shape[0] * len(T)
-
+        sharpness_batch += abs(loss_perturbed - loss_w)            
+        sharpness_batch /= 5 * len(T)
+        #print(sharpness_batch)
         sharpness += sharpness_batch
 
     sharpness /= num_samples
     
-    return sharpness
+    return sharpness.cpu().detach().numpy()
+
+def perturb_model(model, t):
+    nn_model = copy.deepcopy(model)
+    nn_model.eval()
+
+    for param in nn_model.parameters():
+        if param.requires_grad:
+            # Generate random unit vectors of the same shape as param
+            d = torch.randn(1, *param.shape).to(param.device) 
+            # Unsqueeze norm multiple times so that division is done per entry for the first dim of D.
+            d = d / torch.norm(d)
+            d = d.squeeze(0)
+            # Apply the perturbation
+            param.add_(t*d)
+
+    return nn_model
+
+
+
+
+
+
