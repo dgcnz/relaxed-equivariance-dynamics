f50665edc3244bcbc9f2c79f9a0d16cc854a8c44
Diego Canez <canezdiego@gmail.com>
2024-05-27 13:12:25 +0200

    feat: remove unused notebooks

--- scripts/slurm/resume.sh
+++ scripts/slurm/resume.sh
@@ -11,5 +11,7 @@
 cd $HOME/development/dl2
 source .venv/bin/activate
 # run script from above
+
+# sbatch scripts/slurm/resume.sh wang2022/rotation/e2conv 653akrku /gpfs/home6/scur0399/development/dl2/logs/train/runs/2024-05-26_20-59-53/wang2022/653akrku/checkpoints/last.ckpt 12
 srun python -m src.train experiment=$1 +logger.wandb.resume=must logger.wandb.id=$2 ckpt_path=$3 seed=$4
 deactivate
