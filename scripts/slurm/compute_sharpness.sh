9443fd96452c32c9f9d6f989f42aa403022f2dc8
Diego Canez <canezdiego@gmail.com>
2024-05-27 16:21:17 +0200

    fix: compute_sharpness.sh

---
+++ scripts/slurm/compute_sharpness.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --job-name=DL2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=00:10:00
+#SBATCH --output=scripts/slurm_logs/slurm_output_%A.out
+
+cd $HOME/development/dl2
+source .venv/bin/activate
+# run script from above
+srun python -m src.compute_measures_v2 sharpness=True ckpt_path=$1 
+deactivate
